b907897c0a8f4a8929a21fe1cb54301734fe012d
Joel Andersson <joel.andersson@atex.com>
2011-11-24 09:50:25 +0000

    Refactored Service code etc

---
+++ src/main/java/com/polopoly/ps/test/service/Service.java
@@ -0,0 +1,11 @@
+package com.polopoly.ps.test.service;
+
+/**
+ * Marker interface for stuff fetched using {@link ServiceGetter}.
+ * 
+ * @deprecated Moved to {@link com.polopoly.ps.service.Service} in module pcmd
+ */
+@Deprecated
+public interface Service {
+    // marker interface
+}
---
+++ src/main/java/com/polopoly/ps/test/service/ServiceWrapperRelativeOrder.java
@@ -0,0 +1,13 @@
+package com.polopoly.ps.test.service;
+
+/**
+ * @see ServiceWrapper#getWrapperOrder(ServiceWrapper)
+ * 
+ * @deprecated Moved to {@link com.polopoly.ps.service.ServiceWrapperRelativeOrder} in module pcmd.
+ */
+@Deprecated
+public enum ServiceWrapperRelativeOrder {
+
+    I_DONT_CARE, USE_SORT_INDEX;
+
+}
